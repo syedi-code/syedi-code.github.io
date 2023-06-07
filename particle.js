162207afbbd6af0af1112d658a1c1270dca920c6
Ibrahim Syed <syedi@oregonstate.edu>
2023-06-06 22:10:36 -0700

    'Designing Sound for Daxbot' page added and landing page sketch changed

--- particle.js
+++ particle.js
@@ -1,120 +1,133 @@
 class Particle {
-    constructor(x, y, r_) {
-        this.pos = createVector(x, y)
-        this.velocity = p5.Vector.random2D()
-        this.velocity.mult(2)
-        this.r = r_
-        this.m = this.r * 0.1
-
-        this.color = createVector(0, 0, 0)
+    constructor(pos, origin, v) {
+      this.pos = pos;
+      this.v = v;
+      this.angle = 0;
+      
+      this.trail_length = random(50, 100);
+      this.trail_weight = random(0.5, 5); // stroke width
+      this.trailpoint = origin;
+      this.origin = origin.copy();
+      this.attraction_point = createVector(300, 300);
+      
+      this.reached_end = false;
     }
-
-    update() {
-        this.pos.add(this.velocity);
+    
+    // Renders the particle
+    draw() {
+      this.move_towards_point(this.attraction_point);
+      
+      push();
+      this.set_stroke();
+      strokeWeight(this.trail_weight);
+      point(this.pos.x, this.pos.y);
+      this.draw_trail();
+      pop();
+      
+      // this.draw_trailpoint();
+      this.reached_end = this.check_completion();
+      this.update_velocity();
     }
-
-    checkBoundaryCollision() {
-        if (this.pos.x > width - this.r) {
-            this.pos.x = width - this.r
-            this.velocity.x *= -1
-        } else if (this.pos.x < this.r) {
-            this.pos.x = this.r
-            this.velocity.x *= - 1
-        } else if (this.pos.y > height - this.r) {
-            this.pos.y = height - this.r
-            this.velocity.y *= -1
-        } else if (this.pos.y < this.r) {
-            this.pos.y = this.r
-            this.velocity.y *= -1
-        }
+    
+    // Handles particle attraction
+    move_towards_point(point) {
+      var path = createVector(this.pos.x - point.x, this.pos.y - point.y);
+      
+      path.normalize();
+      this.pos.x -= path.x * this.v;
+      this.pos.y -= path.y * this.v;
     }
-
-    checkCollision(other) {
-        var distanceVect = p5.Vector.sub(other.pos, this.pos)
-        var distanceVectMag = distanceVect.mag()
-        var minDistance = this.r + other.r
-
-        push()
-        if (distanceVectMag < minDistance) {
-            this.color.x = random(100, 255);
-            this.color.y = random(100, 255);
-            this.color.z = random(120, 255);
-            other.color.x = random(100, 255);
-            other.color.y = random(100, 255);
-            other.color.z = random(120, 255);
-
-            var distanceCorrection = (minDistance - distanceVectMag) / 2
-            var d = distanceVect.copy()
-            var correctionVector = d.normalize().mult(distanceCorrection)
-            other.pos.add(correctionVector);
-            this.pos.sub(correctionVector);
-
-            // get angle of distanceVect
-            var theta  = distanceVect.heading();
-            // precalculate trig values
-            var sine = sin(theta);
-            var cosine = cos(theta);
-
-            var bTemp = []
-            for (let i = 0; i < 2; i++) {
-                bTemp.push(createVector())
-            }
-
-            bTemp[1].x  = cosine * distanceVect.x + sine * distanceVect.y;
-            bTemp[1].y  = cosine * distanceVect.y - sine * distanceVect.x;
-
-            var vTemp = []
-            for (let i = 0; i < 2; i++) {
-                vTemp.push(createVector())
-            }
-
-            vTemp[0].x  = cosine * this.velocity.x + sine * this.velocity.y;
-            vTemp[0].y  = cosine * this.velocity.y - sine * this.velocity.x;
-            vTemp[1].x  = cosine * other.velocity.x + sine * other.velocity.y;
-            vTemp[1].y  = cosine * other.velocity.y - sine * other.velocity.x;
-
-            var vFinal = []
-            for (let i = 0; i < 2; i++) {
-                vFinal.push(createVector())
-            }
-
-            // final rotated velocity for b[0]
-            vFinal[0].x = ((this.m - other.m) * vTemp[0].x + 2 * other.m * vTemp[1].x) / (this.m + other.m);
-            vFinal[0].y = vTemp[0].y;
-
-            // final rotated velocity for b[0]
-            vFinal[1].x = ((other.m - this.m) * vTemp[1].x + 2 * this.m * vTemp[0].x) / (this.m + other.m);
-            vFinal[1].y = vTemp[1].y;
-
-            // hack to avoid clumping
-            bTemp[0].x += vFinal[0].x;
-            bTemp[1].x += vFinal[1].x;
-
-            var bFinal = []
-            for (let i = 0; i < 2; i++) {
-                bFinal.push(createVector())
-            }
-
-            bFinal[0].x = cosine * bTemp[0].x - sine * bTemp[0].y;
-            bFinal[0].y = cosine * bTemp[0].y + sine * bTemp[0].x;
-            bFinal[1].x = cosine * bTemp[1].x - sine * bTemp[1].y;
-            bFinal[1].y = cosine * bTemp[1].y + sine * bTemp[1].x;
-
-            other.pos.x = this.pos.x + bFinal[1].x;
-            other.pos.y = this.pos.y + bFinal[1].y;
-
-            this.pos.add(bFinal[0])
-            this.velocity.x = cosine * vFinal[0].x - sine * vFinal[0].y;
-            this.velocity.y = cosine * vFinal[0].y + sine * vFinal[0].x;
-            other.velocity.x = cosine * vFinal[1].x - sine * vFinal[1].y;
-            other.velocity.y = cosine * vFinal[1].y + sine * vFinal[1].x;
-        }
-        pop()
+    
+    // Sets particle color and handles color modulation
+    set_stroke() {
+      angleMode(DEGREES);
+      let angle = this.attraction_point.angleBetween(this.origin).toFixed(1);
+      
+      /*
+      *  Top-left corner: 0°
+      *  Top-right corner: -45°
+      *  Bottom-right corner: 0°
+      *  Bottom-left corner: 45°
+      *  Why is it like this? I don't know.
+      */
+      
+      let r = map(angle, -45, 45, 250, 255);
+      let g = map(angle, -45, 45, 0, 255);
+      let b = map(angle, -45, 45, 0, 50);
+      let r_end = map(this.ease_color(), 0, 1, r, 0);
+      let g_end = map(this.ease_color(), 0, 1, g, 0);
+      let b_end = map(this.ease_color(), 0, 1, b, 0);
+      stroke(r_end, g_end, b_end);
     }
-
-    display() {
-        noStroke()
-        fill(this.color.x, this.color.y, this.color.z)
-        ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2)
+    
+    // Applies an easing function to particle progress for color modulation
+    ease_color() {
+      return 1 - pow(1 - this.get_trail_progress(), 3);
     }
-}
+    
+    // Calculates trail progress based on distance from origin and endpoint
+    get_trail_progress() {
+      let dist_from_end = dist(this.trailpoint.x, this.trailpoint.y, this.attraction_point.x, this.attraction_point.y);
+      let total_distance = dist(this.attraction_point.x, this.attraction_point.y, this.origin.x, this.origin.y);
+      let progress = map(dist_from_end, 0, total_distance, 1, 0);
+      
+      return progress;
+    }
+    
+    // Renders particle trail
+    draw_trail() {
+      var dist_start = dist(this.trailpoint.x, this.trailpoint.y, this.pos.x, this.pos.y);
+      var dist_end = dist(this.pos.x, this.pos.y, this.attraction_point.x, this.attraction_point.y);
+      
+      if (dist_start > this.trail_length || dist_end < this.trail_length) {
+        var path = createVector(this.trailpoint.x - this.pos.x, this.trailpoint.y - this.pos.y);
+  
+        path.normalize();
+        this.trailpoint.x -= path.x * this.v;
+        this.trailpoint.y -= path.y * this.v;
+      }
+      
+      line(this.trailpoint.x, this.trailpoint.y, this.pos.x, this.pos.y);
+    }
+    
+    // Not in use - renders the particle's trail startpoint
+    draw_trailpoint() {
+      push();
+      stroke(255, 0, 0);
+      strokeWeight(3);
+      point(this.trailpoint.x, this.trailpoint.y);
+      pop();
+    }
+    
+    // Returns whether the particle has reached its destination or not
+    check_completion() {
+      if (dist(this.trailpoint.x, this.trailpoint.y, this.attraction_point.x, this.attraction_point.y) < 1) {
+        return true;
+      }
+      
+      return false;
+    }
+    
+    // Calculates particle progress based on distance from origin and endpoint
+    get_progress() {
+      let dist_from_end = dist(this.pos.x, this.pos.y, this.attraction_point.x, this.attraction_point.y);
+      let total_distance = dist(this.attraction_point.x, this.attraction_point.y, this.origin.x, this.origin.y);
+      let progress = map(dist_from_end, 0, total_distance, 1, 0);
+      
+      return progress;
+    }
+    
+    // Updates particle velocity based on an easing function and particle progress
+    update_velocity() {
+      let eased_progress = this.ease_in_out_circ(this.get_progress());
+      let min = 1;
+      let max = 10;
+      
+      this.v = eased_progress < 0.5 ? map(eased_progress, 0, 0.5, min, max) : map(eased_progress, 0.5, 1, max, min);
+    }
+    
+    // Easing formula (helper function)
+    ease_in_out_circ(x) {
+      return x < 0.5 ? 2 * x * x : 1 - pow(-2 * x + 2, 2) / 2;
+    }
+  }
--- sketch.js
+++ sketch.js
@@ -1,70 +1,76 @@
-var particles = []
-var numParticles = 10
-var clearButton
+/*
+  BLACK HOLE by Ibrahim Syed
+  
+  The color picker at the bottom of the sketch controls the canvas' background color.
+  The slider next to the color picker controls the size of the black hole.
+  The sliders to the right of the sketch change the particle color ranges.
+*/
 
-let container = document.getElementById('sketch')
+var particles = []
+var num_particles = 144;
+var black_hole_size = 150;
+
+var bg_color_picker;
+var r1, r2, g1, g2, b1, b2;
+var color_sliders = [r1, r2, g1, g2, b1, b2];
+var black_hole_slider;
 
 function setup() {
-  let canvas = createCanvas(900, 550);
-  for (let i = 0; i < numParticles; i++) {
-    particles.push(new Particle(random(0, width), random(0, height), random(4, 15)));
+  createCanvas(600, 600);
+  
+  for (let i = 0; i < num_particles; i++) {
+    particles.push(generate_particle());
   }
-
-  clearButton = createButton('CLEAR CANVAS')
-  clearButton.mousePressed(clearCanvas)
-  canvas.parent(container)
-  clearButton.parent(container)
-  clearButton.style('max-width', width + 'px')
-  clearButton.addClass('clearButton')
-  clearButton.addClass('hover-blue')
-
-  colorMode(HSB, 255);
+  
+  // bg_color_picker = createColorPicker('#fffbe1');
+  // bg_color_picker.position(0, height+5);
+  // this.create_color_sliders();
+  
+  // black_hole_slider = createSlider(0, 500, 150);
+  // black_hole_slider.position(75, height+5);
+  // black_hole_slider.style('width', '120px');
 }
 
 function draw() {
+  push();
   background(255);
-
-  push()
-  stroke(0)
-  strokeWeight(3)
-  noFill()
-  rect(0, 0, width, height)
-  pop()
-
-  for (let p of particles) {
-    p.update()
-    p.display()
-    p.checkBoundaryCollision()
+  stroke(255);
+  fill(0);
+  circle(width/2, height/2, 255);
+  pop();
+  
+  for (let i = particles.length - 1; i > 0; i--) {
+    let p = particles[i];
+    
+    p.draw();
+    if (p.reached_end == true) particles.splice(i, 1);
   }
-
-  for (let i = 0; i < particles.length; i++) {
-    for (let j = 0; j < particles.length; j++) {
-      if (i != j) {
-        particles[i].checkCollision(particles[j]);
-      }
-    }
+  
+  while (particles.length < num_particles) {
+    particles.push(generate_particle()); 
   }
 }
 
-function mousePressed() {
-  var valid = true
+function create_color_sliders() {
+  color_sliders[0] = createSlider(0, 255, 250);  
+  color_sliders[1] = createSlider(0, 255, 255);
+  color_sliders[2] = createSlider(0, 255, 0);
+  color_sliders[3] = createSlider(0, 255, 255);
+  color_sliders[4] = createSlider(0, 255, 0);
+  color_sliders[5] = createSlider(0, 255, 50);
   
-  for (let p of particles) {
-    if ((mouseX < p.pos.x + p.r && mouseX > p.pos.x - p.r) && (mouseY < p.pos.y + p.r && mouseY > p.pos.y - p.r)) {
-      valid = false
-    }
-  }
-
-  if (mouseX > width || mouseY > height || mouseX < 0 || mouseY < 0) {
-    valid = false
-  }
-  
-  if (valid) {
-    particles.push(new Particle(mouseX, mouseY, random(4, 15)));
+  for (let i = 0; i < 6; i++) {
+    let slider = color_sliders[i];
+    
+    slider.position(width+5, i*25);
+    slider.style('width', '80px');
   }
 }
 
-function clearCanvas() {
-  console.log("clear() called")
-  particles.length = 0
+function generate_particle() {
+  let v = 1;
+  let random_pos = createVector(random(0, width), random(0, height));
+  let origin_pos = random_pos.copy();
+  
+  return new Particle(random_pos, origin_pos, v);
 }
